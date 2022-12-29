481bf1d29c598651a8df93184ecc8c57d04e2040
Mario Zechner <badlogicgames@gmail.com>
2022-12-29 21:15:51 +0100

    Mouse and keyboard handling.

--- src/dos/vesa.h
+++ src/dos/vesa.h
@@ -1,11 +1,10 @@
 #pragma once
 
-#include <stdint.h>
 #include <stdbool.h>
-#include <dpmi.h>
+#include <stdint.h>
 
-
-bool vesa_init(uint32_t width, uint32_t height, uint32_t *actual_width, uint32_t *actual_height);
+bool vesa_init(uint32_t width, uint32_t height, uint32_t *actual_width,
+               uint32_t *actual_height);
 
 int vesa_get_frame_buffer_selector();
 
--- tests/dos/dos.c
+++ tests/dos/dos.c
@@ -6,22 +6,39 @@
 #include "gdbstub.h"
 
 int main(void) {
+  int res_x = 640;
+  int res_y = 480;
   gdb_start();
-  uint32_t *pixels = (uint32_t *)malloc(sizeof(uint32_t) * 320 * 240);
-  struct mfb_window *window = mfb_open("Noise Test", 320, 240);
+  uint32_t *pixels = (uint32_t *)malloc(sizeof(uint32_t) * res_x * res_y);
+  struct mfb_window *window = mfb_open("Noise Test", res_x, res_y);
 
   do {
     for (int i = 0; i < 2000; i++) {
-      int x = rand() % 320;
-      int y = rand() % 240;
+      int x = rand() % res_x;
+      int y = rand() % res_y;
       int color = MFB_ARGB(0xff, rand() % 0xff, rand() % 0xff, rand() % 0xff);
-      pixels[x + y * 320] = color;
+      pixels[x + y * res_x] = color;
     }
 
     if (mfb_get_mouse_button_buffer(window)[MOUSE_LEFT]) {
-      memset(pixels, 0, 320 * 240 * 4);
+      int32_t x = mfb_get_mouse_x(window);
+      int32_t y = mfb_get_mouse_y(window);
+      x = x >= res_x ? res_x - 1 : x;
+      x = x < 0 ? 0 : x;
+      y = y >= res_y ? res_y - 1 : y;
+      y = y < 0 ? 0 : y;
+      uint8_t *dst = (uint8_t *)pixels;
+      while (y >= 0) {
+        memset(dst, 0, x * 4);
+        dst += res_x * 4;
+        y--;
+      }
     }
-    mfb_update(window, pixels);
+
+    if (mfb_update(window, pixels) != STATE_OK) {
+      break;
+    }
+
     gdb_checkpoint();
   } while (mfb_wait_sync(window));
   return 0;
